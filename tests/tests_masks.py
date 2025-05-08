0968e1924a3fd54af5f16de614090a1a02663027
Anastasiya <nastysha213@mail.ru>
2025-05-08 22:54:30 +0300

    add test_masks

---
+++ tests/tests_masks.py
@@ -0,0 +1,36 @@
+from src.masks import get_mask_card_number, get_mask_account
+import pytest
+
+def tests_get_mask_card_number():
+    assert get_mask_card_number("1234567890123456") == "1234 56** **** 3456"
+
+    with pytest.raises(ValueError):
+        get_mask_card_number("12345678") == "Номер карты должен содержать минимум 16 цифр"
+
+    with pytest.raises(ValueError):
+        get_mask_card_number("abcd1234") == "Номер карты должен содержать только цифры"
+
+    with pytest.raises(ValueError):
+        get_mask_card_number(" ") == "Номер счёта не может быть пустым"
+
+
+
+def tests_get_mask_account():
+    assert get_mask_account("1234 5678 1234 4567 1234") == "**1234"
+
+    with pytest.raises(ValueError):
+        get_mask_account("123") == "Номер карты должен содержать минимум 16 цифр"
+
+    with pytest.raises(ValueError):
+        get_mask_account("nmh123") == "Номер карты должен содержать только цифры"
+
+    with pytest.raises(ValueError):
+        get_mask_account(" ")  == "Номер счёта не может быть пустым"
+
+
+
+
+
+
+
+
