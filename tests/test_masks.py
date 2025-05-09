1192c4ff54e0be450c4db3607ec4a7abc2085213
Anastasiya <nastysha213@mail.ru>
2025-05-09 13:19:03 +0300

    add test widget

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,7 +1,7 @@
 from src.masks import get_mask_card_number, get_mask_account
 import pytest
 
-def tests_get_mask_card_number():
+def test_get_mask_card_number():
     assert get_mask_card_number("1234567890123456") == "1234 56** **** 3456"
 
     with pytest.raises(ValueError):
@@ -15,7 +15,7 @@ def tests_get_mask_card_number():
 
 
 
-def tests_get_mask_account():
+def test_get_mask_account():
     assert get_mask_account("1234 5678 1234 4567 1234") == "**1234"
 
     with pytest.raises(ValueError):
